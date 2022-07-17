46611de1cd8cd8744a7fb18ea94c9c372c21306f
NAAZIREEN SHAIK <67265911+shaiknaazireen@users.noreply.github.com>
2022-07-17 23:11:40 +0530

    Create tipcalculator.py

---
+++ tipcalculator.py
@@ -0,0 +1,12 @@
+bill=float(input("enter the amount:"))
+tip=int(input("enter the tip percentage:"))
+a=(tip/bill)
+b=(a*100)
+c=int(input("enter number of persons:"))
+amount=((bill+b)/c)
+print(amount)
+print("amount to be given:")
+d=(round(amount,2))
+d="{:.2f}".format(amount)
+print(d)
+
